843bafcf59164731cc341ff366ceddf0d357bbfa
Roberto Eder Weiss Juárez <weiss.uttab@gmail.com>
2016-03-01 15:29:34 -0600

    Guarda en base de datos.

---
+++ MuebleriaSantaCruz/frmLineaProducto.Designer.vb
@@ -0,0 +1,309 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmLineaProducto
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmLineaProducto))
+        Me.BarraHerramientas = New System.Windows.Forms.ToolStrip()
+        Me.btnuevo = New System.Windows.Forms.ToolStripButton()
+        Me.btguardar = New System.Windows.Forms.ToolStripButton()
+        Me.btdeshacer = New System.Windows.Forms.ToolStripButton()
+        Me.bteditar = New System.Windows.Forms.ToolStripButton()
+        Me.btbuscar = New System.Windows.Forms.ToolStripButton()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.cbxActivo = New System.Windows.Forms.CheckBox()
+        Me.lbtipoestado = New System.Windows.Forms.Label()
+        Me.lbestado = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtID = New System.Windows.Forms.TextBox()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.DataSetLineaProducto = New MuebleriaSantaCruz.DataSetLineaProducto()
+        Me.Lineas_productosBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.Lineas_productosTableAdapter = New MuebleriaSantaCruz.DataSetLineaProductoTableAdapters.lineas_productosTableAdapter()
+        Me.TableAdapterManager = New MuebleriaSantaCruz.DataSetLineaProductoTableAdapters.TableAdapterManager()
+        Me.Lineas_productosDataGridView = New System.Windows.Forms.DataGridView()
+        Me.DataGridViewTextBoxColumn1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewCheckBoxColumn1 = New System.Windows.Forms.DataGridViewCheckBoxColumn()
+        Me.ErrorProvider1 = New System.Windows.Forms.ErrorProvider(Me.components)
+        Me.BarraHerramientas.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.DataSetLineaProducto, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Lineas_productosBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Lineas_productosDataGridView, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ErrorProvider1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'BarraHerramientas
+        '
+        Me.BarraHerramientas.BackColor = System.Drawing.SystemColors.Control
+        Me.BarraHerramientas.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.btnuevo, Me.btguardar, Me.btdeshacer, Me.bteditar, Me.btbuscar})
+        Me.BarraHerramientas.Location = New System.Drawing.Point(0, 0)
+        Me.BarraHerramientas.Name = "BarraHerramientas"
+        Me.BarraHerramientas.Size = New System.Drawing.Size(824, 38)
+        Me.BarraHerramientas.TabIndex = 26
+        Me.BarraHerramientas.Text = "ToolStrip1"
+        '
+        'btnuevo
+        '
+        Me.btnuevo.Enabled = False
+        Me.btnuevo.Image = CType(resources.GetObject("btnuevo.Image"), System.Drawing.Image)
+        Me.btnuevo.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.btnuevo.Name = "btnuevo"
+        Me.btnuevo.Size = New System.Drawing.Size(46, 35)
+        Me.btnuevo.Text = "&Nuevo"
+        Me.btnuevo.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
+        '
+        'btguardar
+        '
+        Me.btguardar.Enabled = False
+        Me.btguardar.Image = CType(resources.GetObject("btguardar.Image"), System.Drawing.Image)
+        Me.btguardar.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.btguardar.Name = "btguardar"
+        Me.btguardar.Size = New System.Drawing.Size(53, 35)
+        Me.btguardar.Text = "&Guardar"
+        Me.btguardar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
+        '
+        'btdeshacer
+        '
+        Me.btdeshacer.Enabled = False
+        Me.btdeshacer.Image = CType(resources.GetObject("btdeshacer.Image"), System.Drawing.Image)
+        Me.btdeshacer.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.btdeshacer.Name = "btdeshacer"
+        Me.btdeshacer.Size = New System.Drawing.Size(59, 35)
+        Me.btdeshacer.Text = "&Deshacer"
+        Me.btdeshacer.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
+        '
+        'bteditar
+        '
+        Me.bteditar.Enabled = False
+        Me.bteditar.Image = CType(resources.GetObject("bteditar.Image"), System.Drawing.Image)
+        Me.bteditar.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.bteditar.Name = "bteditar"
+        Me.bteditar.Size = New System.Drawing.Size(41, 35)
+        Me.bteditar.Text = "&Editar"
+        Me.bteditar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
+        '
+        'btbuscar
+        '
+        Me.btbuscar.Enabled = False
+        Me.btbuscar.Image = CType(resources.GetObject("btbuscar.Image"), System.Drawing.Image)
+        Me.btbuscar.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.btbuscar.Name = "btbuscar"
+        Me.btbuscar.Size = New System.Drawing.Size(46, 35)
+        Me.btbuscar.Text = "&Buscar"
+        Me.btbuscar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
+        Me.btbuscar.Visible = False
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.cbxActivo)
+        Me.GroupBox1.Controls.Add(Me.lbtipoestado)
+        Me.GroupBox1.Controls.Add(Me.lbestado)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.txtID)
+        Me.GroupBox1.Controls.Add(Me.txtNombre)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 61)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(307, 192)
+        Me.GroupBox1.TabIndex = 28
+        Me.GroupBox1.TabStop = False
+        '
+        'cbxActivo
+        '
+        Me.cbxActivo.AutoSize = True
+        Me.cbxActivo.Checked = True
+        Me.cbxActivo.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.cbxActivo.Location = New System.Drawing.Point(76, 116)
+        Me.cbxActivo.Name = "cbxActivo"
+        Me.cbxActivo.Size = New System.Drawing.Size(56, 17)
+        Me.cbxActivo.TabIndex = 7
+        Me.cbxActivo.Text = "Activo"
+        Me.cbxActivo.UseVisualStyleBackColor = True
+        '
+        'lbtipoestado
+        '
+        Me.lbtipoestado.AutoSize = True
+        Me.lbtipoestado.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbtipoestado.ForeColor = System.Drawing.Color.Blue
+        Me.lbtipoestado.Location = New System.Drawing.Point(235, 24)
+        Me.lbtipoestado.Name = "lbtipoestado"
+        Me.lbtipoestado.Size = New System.Drawing.Size(0, 13)
+        Me.lbtipoestado.TabIndex = 2
+        Me.lbtipoestado.Visible = False
+        '
+        'lbestado
+        '
+        Me.lbestado.AutoSize = True
+        Me.lbestado.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbestado.ForeColor = System.Drawing.Color.Black
+        Me.lbestado.Location = New System.Drawing.Point(184, 24)
+        Me.lbestado.Name = "lbestado"
+        Me.lbestado.Size = New System.Drawing.Size(50, 13)
+        Me.lbestado.TabIndex = 3
+        Me.lbestado.Text = "Estado:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(6, 54)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(21, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "ID:"
+        '
+        'txtID
+        '
+        Me.txtID.Enabled = False
+        Me.txtID.Location = New System.Drawing.Point(78, 47)
+        Me.txtID.Name = "txtID"
+        Me.txtID.ReadOnly = True
+        Me.txtID.Size = New System.Drawing.Size(54, 20)
+        Me.txtID.TabIndex = 1
+        Me.txtID.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txtNombre
+        '
+        Me.txtNombre.BackColor = System.Drawing.Color.White
+        Me.txtNombre.Enabled = False
+        Me.txtNombre.Location = New System.Drawing.Point(76, 79)
+        Me.txtNombre.MaxLength = 60
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(187, 20)
+        Me.txtNombre.TabIndex = 0
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(6, 79)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(47, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Nombre:"
+        '
+        'DataSetLineaProducto
+        '
+        Me.DataSetLineaProducto.DataSetName = "DataSetLineaProducto"
+        Me.DataSetLineaProducto.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'Lineas_productosBindingSource
+        '
+        Me.Lineas_productosBindingSource.DataMember = "lineas_productos"
+        Me.Lineas_productosBindingSource.DataSource = Me.DataSetLineaProducto
+        '
+        'Lineas_productosTableAdapter
+        '
+        Me.Lineas_productosTableAdapter.ClearBeforeFill = True
+        '
+        'TableAdapterManager
+        '
+        Me.TableAdapterManager.BackupDataSetBeforeUpdate = False
+        Me.TableAdapterManager.Connection = Nothing
+        Me.TableAdapterManager.UpdateOrder = MuebleriaSantaCruz.DataSetLineaProductoTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete
+        '
+        'Lineas_productosDataGridView
+        '
+        Me.Lineas_productosDataGridView.AutoGenerateColumns = False
+        Me.Lineas_productosDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.Lineas_productosDataGridView.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.DataGridViewTextBoxColumn1, Me.DataGridViewTextBoxColumn2, Me.DataGridViewCheckBoxColumn1})
+        Me.Lineas_productosDataGridView.DataSource = Me.Lineas_productosBindingSource
+        Me.Lineas_productosDataGridView.Location = New System.Drawing.Point(335, 61)
+        Me.Lineas_productosDataGridView.Name = "Lineas_productosDataGridView"
+        Me.Lineas_productosDataGridView.Size = New System.Drawing.Size(477, 281)
+        Me.Lineas_productosDataGridView.TabIndex = 29
+        '
+        'DataGridViewTextBoxColumn1
+        '
+        Me.DataGridViewTextBoxColumn1.DataPropertyName = "id"
+        Me.DataGridViewTextBoxColumn1.HeaderText = "id"
+        Me.DataGridViewTextBoxColumn1.Name = "DataGridViewTextBoxColumn1"
+        '
+        'DataGridViewTextBoxColumn2
+        '
+        Me.DataGridViewTextBoxColumn2.DataPropertyName = "nombre"
+        Me.DataGridViewTextBoxColumn2.HeaderText = "nombre"
+        Me.DataGridViewTextBoxColumn2.Name = "DataGridViewTextBoxColumn2"
+        '
+        'DataGridViewCheckBoxColumn1
+        '
+        Me.DataGridViewCheckBoxColumn1.DataPropertyName = "activo"
+        Me.DataGridViewCheckBoxColumn1.HeaderText = "activo"
+        Me.DataGridViewCheckBoxColumn1.Name = "DataGridViewCheckBoxColumn1"
+        '
+        'ErrorProvider1
+        '
+        Me.ErrorProvider1.ContainerControl = Me
+        '
+        'frmLineaProducto
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(824, 354)
+        Me.Controls.Add(Me.Lineas_productosDataGridView)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.BarraHerramientas)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "frmLineaProducto"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Lineas de productos | Mueblería Casa Cruz"
+        Me.BarraHerramientas.ResumeLayout(False)
+        Me.BarraHerramientas.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.DataSetLineaProducto, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Lineas_productosBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Lineas_productosDataGridView, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ErrorProvider1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents BarraHerramientas As System.Windows.Forms.ToolStrip
+    Friend WithEvents btnuevo As System.Windows.Forms.ToolStripButton
+    Friend WithEvents btguardar As System.Windows.Forms.ToolStripButton
+    Friend WithEvents btdeshacer As System.Windows.Forms.ToolStripButton
+    Friend WithEvents bteditar As System.Windows.Forms.ToolStripButton
+    Friend WithEvents btbuscar As System.Windows.Forms.ToolStripButton
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents cbxActivo As System.Windows.Forms.CheckBox
+    Friend WithEvents lbtipoestado As System.Windows.Forms.Label
+    Friend WithEvents lbestado As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtID As System.Windows.Forms.TextBox
+    Friend WithEvents txtNombre As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents DataSetLineaProducto As MuebleriaSantaCruz.DataSetLineaProducto
+    Friend WithEvents Lineas_productosBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents Lineas_productosTableAdapter As MuebleriaSantaCruz.DataSetLineaProductoTableAdapters.lineas_productosTableAdapter
+    Friend WithEvents TableAdapterManager As MuebleriaSantaCruz.DataSetLineaProductoTableAdapters.TableAdapterManager
+    Friend WithEvents Lineas_productosDataGridView As System.Windows.Forms.DataGridView
+    Friend WithEvents DataGridViewTextBoxColumn1 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn2 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewCheckBoxColumn1 As System.Windows.Forms.DataGridViewCheckBoxColumn
+    Friend WithEvents ErrorProvider1 As System.Windows.Forms.ErrorProvider
+End Class
---
+++ MuebleriaSantaCruz/frmLineaProducto.vb
@@ -0,0 +1,269 @@
+﻿Imports System.Data.SqlClient
+
+Public Class frmLineaProducto
+    Private objcon As New Conexion
+    Private Orden As SqlCommand
+    Private Lector As SqlDataReader
+
+    Public Sub EstatusNuevo()
+        If (objcon.con.State = ConnectionState.Closed) Then objcon.con.Open()
+
+        'Crear una consulta
+        Dim Consulta As String = "SELECT id FROM lineas_productos ORDER BY id DESC"
+        Orden = New SqlCommand(Consulta, objcon.con)
+
+
+        Try
+            'ExecuteReader hace la consulta y devuelve un SqlDataReader
+            Lector = Orden.ExecuteReader()
+            Lector.Read()
+
+            If (Lector.HasRows = False) Then
+                txtID.Text = "1"
+                txtNombre.Focus()
+            Else
+                txtID.Text = Lector("id") + 1
+                txtNombre.Focus()
+            End If
+
+            'Llamar siempre a Close una vez finalizada la lectura
+            CerrarConexion()
+        Catch ex As SqlException
+            MsgBox("Error: " + ex.Message)
+            CerrarConexion()
+        End Try
+    End Sub
+
+    Public Sub InsertaNuevoEstatus()
+        MsgBox("Entre a InsertaNuevoEstatus()")
+
+
+        If (objcon.con.State = ConnectionState.Closed) Then objcon.con.Open()
+
+        'Crear una consulta
+        Dim Consulta As String = "INSERT INTO lineas_productos (id, nombre,activo) VALUES (" & txtID.Text & ",'" & txtNombre.Text & "'," & (If(cbxActivo.Checked, 1, 0)) & ")"
+        Orden = New SqlCommand(Consulta, objcon.con)
+
+
+        Try
+            'ExecuteReader hace la consulta y devuelve un SqlDataReader
+            Lector = Orden.ExecuteReader()
+            Lector.Read()
+
+            Me.Lineas_productosTableAdapter.Fill(Me.DataSetLineaProducto.lineas_productos)
+
+            EstadoBotones(True, False, False, False, True)
+            EstadoCajasdeTexto(False)
+
+            For Fila = 0 To Lineas_productosDataGridView.Rows.Count - 1
+                If (Lineas_productosDataGridView.Item(1, Fila).Value = txtNombre.Text) Then
+                    Lineas_productosDataGridView.Item(0, Fila).Selected = True
+                    Exit For
+                End If
+            Next Fila
+
+            'Llamar siempre a Close una vez finalizada la lectura
+            CerrarConexion()
+
+            MessageBox.Show("La linea de producto '" & txtNombre.Text & "' ha sido guardado", "Guardar", MessageBoxButtons.OK)
+
+            LimpiarCajasdeTexto()
+            lbtipoestado.Visible = False
+            Lineas_productosDataGridView.Enabled = True
+        Catch ex As SqlException
+            If (ex.Number = 2601) Then
+                MessageBox.Show("La linea de producto ya existe", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            ElseIf (ex.Number = 2627) Then
+                MessageBox.Show("El ID ya existe", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Else
+                MsgBox("Error: " + ex.Message)
+            End If
+
+            CerrarConexion()
+        End Try
+    End Sub
+
+    Public Sub EditarEstatus()
+        'Crear la conexión con la base de datos
+         If (objcon.con.State = ConnectionState.Closed) Then objcon.con.Open()
+
+        'Crear una consulta
+        Dim Consulta As String = "UPDATE lineas_productos SET nombre = '" & txtNombre.Text & "', activo = " & (If(cbxActivo.Checked, 1, 0)) & " WHERE id = " & txtID.Text
+        Orden = New SqlCommand(Consulta, objcon.con)
+
+
+        Try
+            'ExecuteReader hace la consulta y devuelve un SqlDataReader
+            Lector = Orden.ExecuteReader()
+            Lector.Read()
+
+            Me.Lineas_productosTableAdapter.Fill(Me.DataSetLineaProducto.lineas_productos)
+
+            EstadoBotones(True, False, False, False, True)
+            EstadoCajasdeTexto(False)
+
+            'Llamar siempre a Close una vez finalizada la lectura
+            CerrarConexion()
+
+            MessageBox.Show("La linea de producto '" & txtNombre.Text & "' ha sido guardado", "Guardar", MessageBoxButtons.OK)
+
+            LimpiarCajasdeTexto()
+            lbtipoestado.Visible = False
+            Lineas_productosDataGridView.Enabled = True
+        Catch ex As SqlException
+            If (ex.Number = 2601) Then
+                MessageBox.Show("El estatus ya existe", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            ElseIf (ex.Number = 2627) Then
+                MessageBox.Show("La clave ya existe", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Else
+                MsgBox("Error: " + ex.Message)
+            End If
+
+            CerrarConexion()
+        End Try
+    End Sub
+
+    Public Sub CerrarConexion()
+        ' Cerrar la conexión cuando ya no sea necesaria
+        If (Not Lector Is Nothing) Then
+            Lector.Close()
+        End If
+        If (objcon.con.State = ConnectionState.Open) Then objcon.con.Close()
+    End Sub
+
+    Private Sub LimpiarCajasdeTexto()
+        txtID.Text = ""
+        txtNombre.Text = ""
+        cbxActivo.Checked = True
+    End Sub
+
+    Private Sub EstadoBotones(ByVal nuevo As Boolean, ByVal guardar As Boolean, ByVal deshacer As Boolean, ByVal editar As Boolean, ByVal buscar As Boolean)
+        btnuevo.Enabled = nuevo
+        btguardar.Enabled = guardar
+        btdeshacer.Enabled = deshacer
+        bteditar.Enabled = editar
+        btbuscar.Enabled = buscar
+    End Sub
+
+    Private Sub EstadoCajasdeTexto(ByVal nombre_status As Boolean)
+        txtNombre.Enabled = nombre_status
+        cbxActivo.Enabled = nombre_status
+    End Sub
+
+    Private Sub PegarDatosTabla_CajasdeTexto(ByVal F As Integer)
+        txtID.Text = Lineas_productosDataGridView.Rows(F).Cells(0).Value
+        txtNombre.Text = Lineas_productosDataGridView.Rows(F).Cells(1).Value
+        cbxActivo.Text = Lineas_productosDataGridView.Rows(F).Cells(2).Value
+    End Sub
+
+    Private Sub DesactivarErroresCajasdeTexto()
+        ErrorProvider1.SetError(txtNombre, Nothing)
+    End Sub
+
+    Private Sub Lineas_productosDataGridView_CellClick(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs) Handles Lineas_productosDataGridView.CellClick
+        If (Lineas_productosDataGridView.Enabled = True) Then
+            If (e.RowIndex >= 0) Then
+                PegarDatosTabla_CajasdeTexto(e.RowIndex)
+                EstadoBotones(True, False, True, True, True)
+            End If
+        End If
+    End Sub
+
+    Private Sub Lineas_productosDataGridView_RowEnter(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs) Handles Lineas_productosDataGridView.RowEnter
+        If (Lineas_productosDataGridView.Enabled = True) Then
+            If (e.RowIndex >= 0) Then
+                PegarDatosTabla_CajasdeTexto(e.RowIndex)
+                EstadoBotones(True, False, True, True, True)
+            End If
+        End If
+    End Sub
+
+    Private Sub btnuevo_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnuevo.Click
+        lbtipoestado.Visible = True
+        lbtipoestado.Text = "Nuevo"
+        EstadoBotones(False, True, True, False, False)
+        LimpiarCajasdeTexto()
+        EstadoCajasdeTexto(True)
+        Lineas_productosDataGridView.Enabled = False
+        DesactivarErroresCajasdeTexto()
+        EstatusNuevo()
+    End Sub
+
+    Private Sub btguardar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btguardar.Click
+
+        If (txtNombre.Text.Length = 0) Then
+            ErrorProvider1.SetError(txtNombre, "Capture el nombre de la linea de productos")
+            txtNombre.Focus()
+            Exit Sub
+        End If
+
+        DesactivarErroresCajasdeTexto()
+
+        If (lbtipoestado.Text = "Nuevo") Then
+            InsertaNuevoEstatus()
+        Else
+            EditarEstatus()
+        End If
+    End Sub
+
+    Private Sub btdeshacer_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btdeshacer.Click
+        lbtipoestado.Visible = False
+        lbtipoestado.Text = ""
+
+        LimpiarCajasdeTexto()
+        EstadoCajasdeTexto(False)
+        EstadoBotones(True, False, False, False, True)
+        Lineas_productosDataGridView.Enabled = True
+        DesactivarErroresCajasdeTexto()
+    End Sub
+
+    Private Sub bteditar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles bteditar.Click
+        If (txtID.Text <> "") Then
+            Dim F As Integer = Lineas_productosDataGridView.CurrentCellAddress.Y
+
+            lbtipoestado.Visible = True
+            lbtipoestado.Text = "Editar"
+
+            PegarDatosTabla_CajasdeTexto(F)
+            EstadoCajasdeTexto(True)
+            txtNombre.Focus()
+            EstadoBotones(False, True, True, False, False)
+            Lineas_productosDataGridView.Enabled = False
+        Else
+            MessageBox.Show("Seleccione un estatus para editar", "Editar", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+    End Sub
+
+    Private Sub txtNombre_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtNombre.TextChanged
+        If (txtNombre.Text.Length <> 0) Then
+            DesactivarErroresCajasdeTexto()
+        End If
+    End Sub
+
+    Private Sub txtNombre_KeyPress(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtNombre.KeyPress
+        If (e.KeyChar = "'") Then
+            e.KeyChar = ""
+        End If
+    End Sub
+
+    Private Sub Estatusfrm_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'TODO: esta línea de código carga datos en la tabla 'DataSetLineaProducto.lineas_productos' Puede moverla o quitarla según sea necesario.
+        Me.Lineas_productosTableAdapter.Fill(Me.DataSetLineaProducto.lineas_productos)
+        Me.Top = 85
+
+    End Sub
+
+    Private Sub Estatusfrm_Shown(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Shown
+        'Desactivar para no se active el foco de la tabla de sectores
+        LimpiarCajasdeTexto()
+
+        'Si la tabla sectores esta vacia, deshabilitar el boton de buscar
+        If (Lineas_productosBindingSource.Count = 0) Then
+            EstadoBotones(True, False, False, False, False)
+        Else
+            EstadoBotones(True, False, False, False, True)
+        End If
+
+        Lineas_productosDataGridView.ClearSelection()
+    End Sub
+End Class
