e626310118306a5c5dccc22e97752f1fd04a011a
Roberto Eder Weiss Juárez <weiss.uttab@gmail.com>
2016-03-30 00:08:13 -0600

    .

---
+++ MuebleriaSantaCruz/frmClasificacionCliente.vb
@@ -0,0 +1,266 @@
+﻿Imports System.Data.SqlClient
+
+Public Class frmClasificacionCliente
+    Private objcon As New Conexion
+    Private Orden As SqlCommand
+    Private Lector As SqlDataReader
+
+    Public Sub Nuevo()
+        If (objcon.con.State = ConnectionState.Closed) Then objcon.con.Open()
+
+        'Crear una consulta
+        Dim Consulta As String = "SELECT id FROM clasificaciones_clientes ORDER BY id DESC"
+        Orden = New SqlCommand(Consulta, objcon.con)
+
+
+        Try
+            'ExecuteReader hace la consulta y devuelve un SqlDataReader
+            Lector = Orden.ExecuteReader()
+            Lector.Read()
+
+            If (Lector.HasRows = False) Then
+                txtID.Text = "1"
+                txtNombre.Focus()
+            Else
+                txtID.Text = Lector("id") + 1
+                txtNombre.Focus()
+            End If
+
+            'Llamar siempre a Close una vez finalizada la lectura
+            CerrarConexion()
+        Catch ex As SqlException
+            MsgBox("Error: " + ex.Message)
+            CerrarConexion()
+        End Try
+    End Sub
+
+    Public Sub InsertaNuevo()
+
+        If (objcon.con.State = ConnectionState.Closed) Then objcon.con.Open()
+
+        'Crear una consulta
+        Dim Consulta As String = "INSERT INTO clasificaciones_clientes (id, nombre,activo) VALUES (" & txtID.Text & ",'" & txtNombre.Text & "'," & (If(cbxActivo.Checked, 1, 0)) & ")"
+        Orden = New SqlCommand(Consulta, objcon.con)
+
+
+        Try
+            'ExecuteReader hace la consulta y devuelve un SqlDataReader
+            Lector = Orden.ExecuteReader()
+            Lector.Read()
+
+            Me.Clasificaciones_clientesTableAdapter.Fill(Me.DataSetClasificacionCliente.clasificaciones_clientes)
+
+            EstadoBotones(True, False, False, False, True)
+            EstadoCajasdeTexto(False)
+
+            For Fila = 0 To clasificaciones_clientesDataGridView.Rows.Count - 1
+                If (clasificaciones_clientesDataGridView.Item(1, Fila).Value = txtNombre.Text) Then
+                    clasificaciones_clientesDataGridView.Item(0, Fila).Selected = True
+                    Exit For
+                End If
+            Next Fila
+
+            'Llamar siempre a Close una vez finalizada la lectura
+            CerrarConexion()
+
+            MessageBox.Show("La linea de producto '" & txtNombre.Text & "' ha sido guardado", "Guardar", MessageBoxButtons.OK)
+
+            LimpiarCajasdeTexto()
+            lbtipoestado.Visible = False
+            clasificaciones_clientesDataGridView.Enabled = True
+        Catch ex As SqlException
+            If (ex.Number = 2601) Then
+                MessageBox.Show("La linea de producto ya existe", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            ElseIf (ex.Number = 2627) Then
+                MessageBox.Show("El ID ya existe", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Else
+                MsgBox("Error: " + ex.Message)
+            End If
+
+            CerrarConexion()
+        End Try
+    End Sub
+
+    Public Sub Editar()
+        'Crear la conexión con la base de datos
+        If (objcon.con.State = ConnectionState.Closed) Then objcon.con.Open()
+
+        'Crear una consulta
+        Dim Consulta As String = "UPDATE clasificaciones_clientes SET nombre = '" & txtNombre.Text & "', activo = " & (If(cbxActivo.Checked, 1, 0)) & " WHERE id = " & txtID.Text
+        Orden = New SqlCommand(Consulta, objcon.con)
+
+
+        Try
+            'ExecuteReader hace la consulta y devuelve un SqlDataReader
+            Lector = Orden.ExecuteReader()
+            Lector.Read()
+
+            Me.Clasificaciones_clientesTableAdapter.Fill(Me.DataSetClasificacionCliente.clasificaciones_clientes)
+
+            EstadoBotones(True, False, False, False, True)
+            EstadoCajasdeTexto(False)
+
+            'Llamar siempre a Close una vez finalizada la lectura
+            CerrarConexion()
+
+            MessageBox.Show("La clasificación de cliente '" & txtNombre.Text & "' ha sido guardado", "Guardar", MessageBoxButtons.OK)
+
+            LimpiarCajasdeTexto()
+            lbtipoestado.Visible = False
+            clasificaciones_clientesDataGridView.Enabled = True
+        Catch ex As SqlException
+            If (ex.Number = 2601) Then
+                MessageBox.Show("La clasificación de cliente ya existe", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            ElseIf (ex.Number = 2627) Then
+                MessageBox.Show("El ID ya existe", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Else
+                MsgBox("Error: " + ex.Message)
+            End If
+
+            CerrarConexion()
+        End Try
+    End Sub
+
+    Public Sub CerrarConexion()
+        ' Cerrar la conexión cuando ya no sea necesaria
+        If (Not Lector Is Nothing) Then
+            Lector.Close()
+        End If
+        If (objcon.con.State = ConnectionState.Open) Then objcon.con.Close()
+    End Sub
+
+    Private Sub LimpiarCajasdeTexto()
+        txtID.Text = ""
+        txtNombre.Text = ""
+        cbxActivo.Checked = True
+    End Sub
+
+    Private Sub EstadoBotones(ByVal nuevo As Boolean, ByVal guardar As Boolean, ByVal deshacer As Boolean, ByVal editar As Boolean, ByVal buscar As Boolean)
+        btnuevo.Enabled = nuevo
+        btguardar.Enabled = guardar
+        btdeshacer.Enabled = deshacer
+        bteditar.Enabled = editar
+        btbuscar.Enabled = buscar
+    End Sub
+
+    Private Sub EstadoCajasdeTexto(ByVal nombre_status As Boolean)
+        txtNombre.Enabled = nombre_status
+        cbxActivo.Enabled = nombre_status
+    End Sub
+
+    Private Sub PegarDatosTabla_CajasdeTexto(ByVal F As Integer)
+        txtID.Text = clasificaciones_clientesDataGridView.Rows(F).Cells(0).Value
+        txtNombre.Text = clasificaciones_clientesDataGridView.Rows(F).Cells(1).Value
+        cbxActivo.Checked = Clasificaciones_clientesDataGridView.Rows(F).Cells(2).Value
+    End Sub
+
+    Private Sub DesactivarErroresCajasdeTexto()
+        ErrorProvider1.SetError(txtNombre, Nothing)
+    End Sub
+
+    Private Sub clasificaciones_clientesDataGridView_CellClick(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs) Handles Clasificaciones_clientesDataGridView.CellClick
+        If (clasificaciones_clientesDataGridView.Enabled = True) Then
+            If (e.RowIndex >= 0) Then
+                PegarDatosTabla_CajasdeTexto(e.RowIndex)
+                EstadoBotones(True, False, True, True, True)
+            End If
+        End If
+    End Sub
+
+    Private Sub clasificaciones_clientesDataGridView_RowEnter(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs) Handles Clasificaciones_clientesDataGridView.RowEnter
+        If (clasificaciones_clientesDataGridView.Enabled = True) Then
+            If (e.RowIndex >= 0) Then
+                PegarDatosTabla_CajasdeTexto(e.RowIndex)
+                EstadoBotones(True, False, True, True, True)
+            End If
+        End If
+    End Sub
+
+    Private Sub btnuevo_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnuevo.Click
+        lbtipoestado.Visible = True
+        lbtipoestado.Text = "Nuevo"
+        EstadoBotones(False, True, True, False, False)
+        LimpiarCajasdeTexto()
+        EstadoCajasdeTexto(True)
+        clasificaciones_clientesDataGridView.Enabled = False
+        DesactivarErroresCajasdeTexto()
+        Nuevo()
+    End Sub
+
+    Private Sub btguardar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btguardar.Click
+
+        If (txtNombre.Text.Length = 0) Then
+            ErrorProvider1.SetError(txtNombre, "Capture el nombre de la clasificación de cliente")
+            txtNombre.Focus()
+            Exit Sub
+        End If
+
+        DesactivarErroresCajasdeTexto()
+
+        If (lbtipoestado.Text = "Nuevo") Then
+            InsertaNuevo()
+        Else
+            Editar()
+        End If
+    End Sub
+
+    Private Sub btdeshacer_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btdeshacer.Click
+        lbtipoestado.Visible = False
+        lbtipoestado.Text = ""
+
+        LimpiarCajasdeTexto()
+        EstadoCajasdeTexto(False)
+        EstadoBotones(True, False, False, False, True)
+        clasificaciones_clientesDataGridView.Enabled = True
+        DesactivarErroresCajasdeTexto()
+    End Sub
+
+    Private Sub bteditar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles bteditar.Click
+        If (txtID.Text <> "") Then
+            Dim F As Integer = clasificaciones_clientesDataGridView.CurrentCellAddress.Y
+
+            lbtipoestado.Visible = True
+            lbtipoestado.Text = "Editar"
+
+            PegarDatosTabla_CajasdeTexto(F)
+            EstadoCajasdeTexto(True)
+            txtNombre.Focus()
+            EstadoBotones(False, True, True, False, False)
+            clasificaciones_clientesDataGridView.Enabled = False
+        Else
+            MessageBox.Show("Seleccione una clasificación de cliente para editar", "Editar", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+    End Sub
+
+    Private Sub txtNombre_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtNombre.TextChanged
+        If (txtNombre.Text.Length <> 0) Then
+            DesactivarErroresCajasdeTexto()
+        End If
+    End Sub
+
+    Private Sub txtNombre_KeyPress(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtNombre.KeyPress
+        If (e.KeyChar = "'") Then
+            e.KeyChar = ""
+        End If
+    End Sub
+
+    Private Sub frmClasificacionCliente_Shown(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Shown
+        'Desactivar para no se active el foco de la tabla de sectores
+        LimpiarCajasdeTexto()
+
+        'Si la tabla sectores esta vacia, deshabilitar el boton de buscar
+        If (Clasificaciones_clientesBindingSource.Count = 0) Then
+            EstadoBotones(True, False, False, False, False)
+        Else
+            EstadoBotones(True, False, False, False, True)
+        End If
+
+        Clasificaciones_clientesDataGridView.ClearSelection()
+    End Sub
+    Private Sub frmClasificacionCliente_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: esta línea de código carga datos en la tabla 'DataSetClasificacionCliente.clasificaciones_clientes' Puede moverla o quitarla según sea necesario.
+        Me.Clasificaciones_clientesTableAdapter.Fill(Me.DataSetClasificacionCliente.clasificaciones_clientes)
+        Me.Top = 85
+
+    End Sub
+End Class
---
+++ MuebleriaSantaCruz/frmLogin.Designer.vb
@@ -0,0 +1,155 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmLogin
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmLogin))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtUsuario = New System.Windows.Forms.TextBox()
+        Me.txtContrasena = New System.Windows.Forms.TextBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.btnAceptar = New System.Windows.Forms.Button()
+        Me.btnCancelar = New System.Windows.Forms.Button()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 29)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(69, 19)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Usuario:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(12, 67)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(102, 19)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Contraseña:"
+        '
+        'txtUsuario
+        '
+        Me.txtUsuario.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtUsuario.Location = New System.Drawing.Point(132, 25)
+        Me.txtUsuario.MaxLength = 15
+        Me.txtUsuario.Name = "txtUsuario"
+        Me.txtUsuario.Size = New System.Drawing.Size(169, 27)
+        Me.txtUsuario.TabIndex = 1
+        '
+        'txtContrasena
+        '
+        Me.txtContrasena.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtContrasena.Location = New System.Drawing.Point(132, 67)
+        Me.txtContrasena.MaxLength = 15
+        Me.txtContrasena.Name = "txtContrasena"
+        Me.txtContrasena.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtContrasena.Size = New System.Drawing.Size(169, 27)
+        Me.txtContrasena.TabIndex = 3
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = Global.MuebleriaCasaCruz.My.Resources.Resources._1456965038_Password
+        Me.PictureBox2.Location = New System.Drawing.Point(319, 62)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(32, 32)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox2.TabIndex = 7
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.MuebleriaCasaCruz.My.Resources.Resources._1456964951_photo
+        Me.PictureBox1.Location = New System.Drawing.Point(319, 20)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(32, 32)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox1.TabIndex = 6
+        Me.PictureBox1.TabStop = False
+        '
+        'btnAceptar
+        '
+        Me.btnAceptar.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAceptar.Image = Global.MuebleriaCasaCruz.My.Resources.Resources._1456964917_ok_circle
+        Me.btnAceptar.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btnAceptar.Location = New System.Drawing.Point(187, 117)
+        Me.btnAceptar.Name = "btnAceptar"
+        Me.btnAceptar.Size = New System.Drawing.Size(169, 44)
+        Me.btnAceptar.TabIndex = 5
+        Me.btnAceptar.Text = "Aceptar"
+        Me.btnAceptar.UseVisualStyleBackColor = True
+        '
+        'btnCancelar
+        '
+        Me.btnCancelar.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCancelar.Image = Global.MuebleriaCasaCruz.My.Resources.Resources._1456964714_Cancel
+        Me.btnCancelar.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btnCancelar.Location = New System.Drawing.Point(12, 117)
+        Me.btnCancelar.Name = "btnCancelar"
+        Me.btnCancelar.Size = New System.Drawing.Size(169, 44)
+        Me.btnCancelar.TabIndex = 4
+        Me.btnCancelar.Text = "Cancelar"
+        Me.btnCancelar.UseVisualStyleBackColor = True
+        '
+        'frmLogin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.ClientSize = New System.Drawing.Size(369, 182)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.btnAceptar)
+        Me.Controls.Add(Me.btnCancelar)
+        Me.Controls.Add(Me.txtContrasena)
+        Me.Controls.Add(Me.txtUsuario)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "frmLogin"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Login | Mueblería Casa Cruz"
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtUsuario As System.Windows.Forms.TextBox
+    Friend WithEvents txtContrasena As System.Windows.Forms.TextBox
+    Friend WithEvents btnCancelar As System.Windows.Forms.Button
+    Friend WithEvents btnAceptar As System.Windows.Forms.Button
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+
+End Class
